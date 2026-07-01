e3b71ddf5922e1ece0fc09866b447b5782bf4ba3
Aryan <aryanbrite@gmail.com>
2026-07-01 19:22:48 +0530

    added compose js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
-const app = express()
+const app = express();
 const PORT=3000;
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.sendFile(index.html);
+});
+app.post("/send-email", (req,res) => {
+    console.log(req.body);
+    res.send("recived!");
+});
 app.listen(PORT, () => {
-    console.log(`server is running at localhost:${PORT}`)
-})
+    console.log(`server is running at localhost:${PORT}`);
+});
