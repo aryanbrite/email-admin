25da62dbeac5dce8a86beab6f12c4cf4b7b64db6
Aryan <aryanbrite@gmail.com>
2026-07-01 19:34:08 +0530

    taken values

--- compose.js
+++ compose.js
@@ -1,1 +1,10 @@
-const button = document.getElementById("sendBtn")
+const button = document.getElementById("sendBtn");
+const to = document.getElementById("rec1");
+const subject = document.getElementById("send111");
+const text = document.getElementById("text1");
+
+button.onclick = function () {
+    console.log(to.value);
+    console.log(subject.value);
+    console.log(text.value);
+};
