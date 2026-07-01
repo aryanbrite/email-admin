880ae1cdb8bfc96aaceb67d55bbb98047c157b72
Aryan <aryanbrite@gmail.com>
2026-07-01 19:29:47 +0530

    added frountend to server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT=3000;
+app.use(express.static(__dirname));
 app.use(express.json());
 app.get("/", (req, res) => {
-    res.sendFile(index.html);
+    res.sendFile(path.join(__dirname,"index.html"));
 });
 app.post("/send-email", (req,res) => {
     console.log(req.body);
