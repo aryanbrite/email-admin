aa91d1a9cf2b7fead0fd402c9fcc671d2b5c0958
Aryan <aryanbrite@gmail.com>
2026-07-05 23:22:10 +0530

    added email check frontend

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const send = document.getElementById("send")
+const prev1 = document.getElementById("prev1")
 send.onclick = function() {
   window.location.href = "compose.html";
 };
+prev1.onclick = function() {
+  window.location.href = "eprev.html";
+}
